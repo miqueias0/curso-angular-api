37f8bb5911b1cf14a3fba87f8bf68a8ddc9ffc77
miqueias0 <miqueiasnadaluti5@gmail.com>
2021-07-22 09:48:28 -0300

    master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { PainelComponent } from './painel/painel.component';
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'painel', component: PainelComponent},
-  {path: '**', pathMatch:'full', redirectTo: 'login'}
+  {path: '**', pathMatch:'full'}
 ];
 
 @NgModule({
